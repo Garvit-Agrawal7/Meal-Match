59e7b462a81a58b9cc8cba59f76a4635a284e93b
Garvit-Agrawal7 <garvitagrawal75@gmail.com>
2023-09-05 20:38:23 +0530

    Backend Commit

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended:true }));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs")
+const API_URL = "https://www.themealdb.com/api/json/v1/1/"
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(`${API_URL}search.php?s=Pasta`)
+        const result = JSON.stringify(response.data.meals[0].strInstructions)
+    } catch (error) {
+        res.status(404)
+    }
 });
 
 app.listen(port)
