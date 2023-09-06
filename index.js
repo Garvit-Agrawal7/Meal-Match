dbf5a663720f37971a94499a352b07270ecfaa0c
Garvit-Agrawal7 <garvitagrawal75@gmail.com>
2023-09-06 17:24:34 +0530

    Functioning Web-App

--- index.js
+++ index.js
@@ -11,12 +11,21 @@ app.use(bodyParser.urlencoded({ extended:true }));
 const API_URL = "https://www.themealdb.com/api/json/v1/1/"
 
 app.get("/", async (req, res) => {
+    res.render("index.ejs")
+});
+
+app.post("/", async (req, res) => {
+    const dishName = req.body.dish;
     try {
-        const response = await axios.get(`${API_URL}search.php?s=Pasta`)
-        const result = JSON.stringify(response.data.meals[0].strInstructions)
+        const response = await axios.get(`${API_URL}search.php?s=${dishName}`)
+        const result = response.data.meals[0].strInstructions
+        res.render("index.ejs", {
+            dish: dishName,
+            recipe: result
+        })
     } catch (error) {
         res.status(404)
     }
-});
+})
 
 app.listen(port)
