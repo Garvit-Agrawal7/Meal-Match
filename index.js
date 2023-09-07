1b63c8e6592220e98748f69278a537ed8a8bef5c
Garvit-Agrawal7 <garvitagrawal75@gmail.com>
2023-09-07 12:44:18 +0530

    Button that generates Random Recipes, Added Ingredients list

--- index.js
+++ index.js
@@ -16,16 +16,55 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
     const dishName = req.body.dish;
-    try {
-        const response = await axios.get(`${API_URL}search.php?s=${dishName}`)
-        const result = response.data.meals[0].strInstructions
-        res.render("index.ejs", {
-            dish: dishName,
-            recipe: result
-        })
-    } catch (error) {
-        res.status(404)
+    let ingredients = [];
+    if (req.body.submit) {
+        try {
+            const response = await axios.get(`${API_URL}search.php?s=${dishName}`)
+            const result = response.data.meals[0].strInstructions
+            const meal = response.data.meals[0];
+            for (let i = 1; i <= 19; i++) {
+                const ingredientKey = `strIngredient${i}`;
+                const measureKey = `strMeasure${i}`;
+                const ingredientValue = meal[ingredientKey];
+                const measureValue = meal[measureKey];
+                if (ingredientValue) {
+                    let ingredient = `${ingredientValue}: ${measureValue}`;
+                    ingredients.push(ingredient)
+                }
+            }
+            res.render("index.ejs", {
+                dish: dishName,
+                recipe: result,
+                ingredients: ingredients
+            })
+        } catch (error) {
+            res.status(404)
+        }
+    } else if (req.body.random) {
+        try {
+            const response = await axios.get(`${API_URL}random.php`)
+            const result = response.data.meals[0].strInstructions;
+            const randDish = response.data.meals[0].strMeal;
+            const meal = response.data.meals[0];
+            for (let i = 1; i <= 19; i++) {
+                const ingredientKey = `strIngredient${i}`;
+                const measureKey = `strMeasure${i}`;
+                const ingredientValue = meal[ingredientKey];
+                const measureValue = meal[measureKey];
+                if (ingredientValue) {
+                    let ingredient = `${ingredientValue}: ${measureValue}`;
+                    ingredients.push(ingredient)
+                }
+            }
+            res.render("index.ejs", {
+                dish: randDish,
+                recipe: result,
+                ingredients: ingredients
+            })
+        } catch (error) {
+            res.status(404)
+        }
     }
-})
+});
 
 app.listen(port)
